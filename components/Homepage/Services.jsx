dddb2ac71590cf77d7f47f038e8c4139cce51ae1
ProMinato500 <nandip500@gmail.com>
2022-06-13 09:22:30 +0530

    finished

--- components/Homepage/Services.jsx
+++ components/Homepage/Services.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Services = () => {
   return (
     <div className='flex flex-wrap sm:flex-nowrap sm:w-4/5 justify-center h-[650px] overflow-hidden m-auto sm:justify-between mt-20'>
-      <Link href='/web_design'>
+      <Link href='/'>
         <div className='cursor-pointer w-full sm:w-1/2 m-5  bg-[url("/images/image-web-design-large.jpg")] bg-no-repeat bg-cover rounded-xl flex'>
             <div className='w-fit h-fit m-auto text-white text-center'>
               <h1 className='text-5xl tracking-[2px]'>WEB DESIGN</h1>
@@ -13,7 +13,7 @@ const Services = () => {
         </div>
       </Link>
       <div className='sm:w-1/2 sm:ml-0 m-5 mt-0'>
-        <Link href='/app_design'>
+        <Link href='/'>
           <div className='cursor-pointer w-full mb-5 h-1/2 bg-[url("/images/image-app-design.jpg")] bg-no-repeat bg-cover rounded-xl flex '>
             <div className='w-fit h-fit m-auto text-white text-center'>
               <h1 className='text-5xl tracking-[2px]'>APP DESIGN</h1>
@@ -21,7 +21,7 @@ const Services = () => {
             </div>
           </div>
         </Link>
-        <Link href='/graphic_design'>
+        <Link href='/'>
           <div className='cursor-pointer w-full h-1/2 bg-[url("/images/image-graphic-design.jpg")] bg-no-repeat bg-cover rounded-xl flex'>
             <div className='w-fit h-fit m-auto text-white  text-center'>
               <h1 className='text-5xl tracking-[2px]'>GRAPHIC DESIGN</h1>
